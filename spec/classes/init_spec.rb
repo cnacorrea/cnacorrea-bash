ebab7372386c83d89be8069d085320bf54b62648
renanvicente <renanvice@gmail.com>
2014-04-26 01:32:42 -0300

    test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,14 @@
-#require 'spec_helper'
-require "#{File.join(File.dirname(__FILE__),'..','spec_helper.rb')}"
+#require "#{File.join(File.dirname(__FILE__),'..','spec_helper.rb')}"
+require 'spec_helper'
 
-describe "bash" do
-  it { should create_class('bash')}
+describe "bash", :type => :class  do
+  let(:title) { 'bash' }
+  let(:node) { 'rspec.renanvicente.com' }
+  let :facts do
+    { 
+      :ipaddress => '10.10.30.30',
+      :concat_basedir => '/specbash',
+    }
+  end
+  it { should contain_class("bash") }
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,24 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-system/spec_helper'
+require 'rspec-system-puppet/helpers'
+
+include RSpecSystemPuppet::Helpers
+
+
+RSpec.configure do |c|
+  # Project root
+  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+  # Readable test descriptions
+  c.formatter = :documentation
+
+  # Configure all nodes in nodeset
+  c.before :suite do
+  shell 'puppet module install puppetlabs/concat'
+#    # Install module and dependencies
+#    puppet_module_install(:source => proj_root, :module_name => 'cnacorrea-bash')
+#    hosts.each do |host|
+#      on host, puppet('module','install','puppetlabs-concat'), { :acceptable_exit_codes => [0,1] }
+#    end
+  end
+end

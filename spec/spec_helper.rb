096b573e33c5624eda54326186c163c6f621f9dd
renanvicente <renanvice@gmail.com>
2014-05-06 02:22:09 -0300

    add spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,1 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
-#require 'rspec-system/spec_helper'
-#require 'rspec-system-puppet/helpers'
-
-#include RSpecSystemPuppet::Helpers
-#
-#
-#RSpec.configure do |c|
-#  # Project root
-#  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-#
-#  # Configure all nodes in nodeset
-#  c.before :suite do
-#    puppet_module_install(:source => proj_root + '/spec/fixtures/modules/concat', :module_name => 'concat')
-#    shell 'puppet module install puppetlabs/concat'
-#  end
-#end

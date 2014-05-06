bda1e04be0ebd5c62c2f989d6df4f4a2c1d7f72b
renanvicente <renanvice@gmail.com>
2014-05-06 02:40:36 -0300

    changed spec init

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,14 +1,13 @@
 #require "#{File.join(File.dirname(__FILE__),'..','spec_helper.rb')}"
 require 'spec_helper'
 
-describe "bash", :type => :class  do
-  let(:title) { 'bash' }
-  let(:node) { 'rspec.renanvicente.com' }
-  let :facts do
-    { 
-      :ipaddress => '10.10.30.30',
-      :concat_basedir => '/specbash',
-    }
-  end
-#  it { should contain_class("bash") }
+describe 'bash' do
+	let(:title) { 'bash' }
+	let(:node) { 'rspec.renanvicente.com' }
+	let(:facts) { { :ipaddress => '10.10.20.20' } }
+
+	describe 'Test standard configuration' do
+		should contain_concat__fragment('cnacorrea-bash_header').with( 'target' => '/etc/profile.d/cnacorrea-bash.sh' )
+		it { should contain_class('bash') }
+	end
 end
